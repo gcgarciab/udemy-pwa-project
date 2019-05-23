43c8dd9f65fa220e5616041f4e3057d0caec8f5e
Camilo García <gcgarciab@slabcode.com>
2019-05-23 15:11:40 -0500

    Reacting to Incoming Events (in sw)

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,7 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function() {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function() {
     console.log('Service worker registered'); 
   })
 }
